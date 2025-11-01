1b05558b3d6ec70b9bc4853fbef5ad69fb27bf8b
Ali <alinmircea10@gmail.com>
2025-11-01 20:53:17 +0200

    "Initial Spring Boot project setup"

---
+++ src/main/java/com/clinica/management/ClinicaManagementSystemApplication.java
@@ -0,0 +1,4 @@
+package com.clinica.management;
+
+public class ClinicaManagementSystemApplication {
+}
---
+++ src/test/java/com/clinica/management/ClinicaManagementSystemApplicationTests.java
@@ -0,0 +1,14 @@
+package com.clinica.management;
+
+import org.junit.Test;
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@com.clinica.management.SpringBootTest
+public class ClinicaManagementSystemApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
